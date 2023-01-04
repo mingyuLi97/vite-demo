687582fec1280166b7673fb46d23c876bf41f265
limy <mingyuli1997@gmail.com>
2023-01-04 19:43:09 +0800

    feat: css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, normalizePath } from "vite";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+
+// 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
+import path from "path";
+
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve("./src/assets/variable.scss"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ["safari >= 6", "ff > 31", "ie 11"],
+        }),
+      ],
+    },
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
+  plugins: [vue()],
+});
